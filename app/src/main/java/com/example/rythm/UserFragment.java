214a17520b15a04d1f2793a55c0074bc74276174
ReyVega <A01114523@itesm.mx>
2021-05-12 00:53:08 -0500

    Adds Change Data screen and modifies the main bottom bar
    Signed-off-by: ReyVega <A01114523@itesm.mx>

--- app/src/main/java/com/example/rythm/UserFragment.java
+++ app/src/main/java/com/example/rythm/UserFragment.java
@@ -3,12 +3,19 @@ package com.example.rythm;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class UserFragment extends Fragment {
+
+    private static final String TAG_FRAGMENT = "fragment";
+    private Button btnEditUserData;
+    private EditUserDataFragment editUserDataFragment;
 
     public UserFragment() {
         // Required empty public constructor
@@ -22,7 +29,17 @@ public class UserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_user, container, false);
+        this.btnEditUserData = view.findViewById(R.id.btnEditUserData);
+        this.editUserDataFragment = new EditUserDataFragment();
+
+        this.btnEditUserData.setOnClickListener(v -> {
+            FragmentManager mr = getFragmentManager();
+            FragmentTransaction transaction = mr.beginTransaction();
+            transaction.replace(R.id.container, this.editUserDataFragment, TAG_FRAGMENT);
+            transaction.commit();
+        });
+
+        return view;
     }
 }
