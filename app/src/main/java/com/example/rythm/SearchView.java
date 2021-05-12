73fa6aea17426e89f2344f5a3ebd1429c2ed204e
ReyVega <A01114523@itesm.mx>
2021-05-12 02:40:29 -0500

    Adds SearchView
    Signed-off-by: ReyVega <A01114523@itesm.mx>

---
+++ app/src/main/java/com/example/rythm/SearchView.java
@@ -0,0 +1,14 @@
+package com.example.rythm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_view);
+    }
+}
