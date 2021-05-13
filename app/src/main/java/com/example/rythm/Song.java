e5a1c1b73727f0df3e9f284118320cfd4d4d6e48
AgusQuintanar <agusquintanar17@gmail.com>
2021-05-12 23:00:00 -0500

    adds functionality to fetch songs from Firebase and their metadata from Deezer API

--- app/src/main/java/com/example/rythm/Song.java
+++ app/src/main/java/com/example/rythm/Song.java
@@ -1,17 +1,23 @@
 package com.example.rythm;
 
+import android.annotation.SuppressLint;
+
 public class Song {
     private String  songName,
                     artistName,
-                    genreName,
+                    coverUrl,
                     userId;
+
+
+    private long duration;
 
     public Song() {}; //Empty constructor for FireStore
 
-    public Song(String songName, String artistName, String genreName, String userId) {
+    public Song(String songName, String artistName, long duration, String coverUrl, String userId) {
         this.songName = songName;
         this.artistName = artistName;
-        this.genreName = genreName;
+        this.duration = duration;
+        this.coverUrl = coverUrl;
         this.userId = userId;
     }
 
@@ -32,15 +38,35 @@ public class Song {
         this.artistName = artistName;
     }
 
-    public String getGenreName() {
-        return genreName;
-    }
-
-    public void setGenreName(String genreName) {
-        this.genreName = genreName;
-    }
 
     public String getUserId() { return userId; }
 
     public void setUserId(String userId) { this.userId = userId; }
+
+    public long getDuration() {
+        return duration;
+    }
+
+
+    private String formatDuration(long duration) {
+        if (duration < 0 || duration >= 3600) return "00:00";
+        long minutes = duration / 60,
+                seconds = duration % 60;
+        return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+    }
+    public String getFormattedDuration() {
+        return formatDuration(this.duration);
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
 }
